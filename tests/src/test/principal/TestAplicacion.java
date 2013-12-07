aac9e5d3646a38ed526f29fbb16d1d61ab93bda4
PaytonZ <jlpv_90@hotmail.com>
2013-12-07 21:11:33 +0100

    Added app test.

---
+++ tests/src/test/principal/TestAplicacion.java
@@ -0,0 +1,15 @@
+package test.principal;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import test.integracion.TestsIntegracionSuite;
+import test.negocio.TestNegocioSuite;
+import test.negocio.clientes.servicioaplicacion.imp.TestSAClientesImp;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestNegocioSuite.class , TestsIntegracionSuite.class })
+public class TestAplicacion {
+
+}
