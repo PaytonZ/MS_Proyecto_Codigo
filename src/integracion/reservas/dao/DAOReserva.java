f1b76d9db48e2e051d5b216a53b9eb476910717b
Zabala <Alex@zbl.es>
2013-12-07 20:30:54 +0100

    Dao reservas acabado
    // cambiar cojer todas las reservas , pedir por parametro un
    transfercliente y dar todas las reservas de un cliente
    
    // al insertar una nueva reserva , al devolver la id de la reserva es
    necesario hacer una consulta con dni + fecha de
    la consulta , luego dni + fecha tienen que formar clave unica
    
    //borrado getID(), setdni()  setid() y setnumerohabitacion() y
    fechareserva(), fecha entrada() fechasalida() del transferreservas
    (motivo no tienen parametro de entrada y en un set es un poco triste)
    //cambiar tambien la interfaz de manera que getallclientes necesite un
    integer del id del cliente
    // daoreservaimp no importa transfercliente

--- src/integracion/reservas/dao/DAOReserva.java
+++ src/integracion/reservas/dao/DAOReserva.java
@@ -43,7 +43,7 @@ public interface DAOReserva {
 	 * @generated 
 	 *            "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public List<TransferReserva> getAllReservas();
+	public List<TransferReserva> getAllReservas(Integer idCliente);
 
 	/**
 	 * <!-- begin-UML-doc --> <!-- end-UML-doc -->
