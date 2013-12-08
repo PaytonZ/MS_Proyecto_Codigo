4402150e7be48b4ce8e7ef707a6bf994cd6e35d6
Plandevida <plandevida@planes.com>
2013-12-08 01:15:06 +0100

    Añadida carpeta para imagenes. FrameAltaClientes creado con el Window Builder Pro

--- src/presentacion/clientes/frames/FrameAltaClientes.java
+++ src/presentacion/clientes/frames/FrameAltaClientes.java
@@ -1,29 +1,24 @@
-/**
- * 
- */
 package presentacion.clientes.frames;
+
+import javax.swing.JPanel;
 
 import presentacion.clientes.GUIInterfazClientes;
 
-/**
- * <!-- begin-UML-doc --> <!-- end-UML-doc -->
- * 
- * @author NASAIII
- * @generated 
- *            "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
-public class FrameAltaClientes implements GUIInterfazClientes {
-	/**
-	 * (sin Javadoc)
-	 * 
-	 * @see GUIInterfazClientes#actualizarVentana(Object datos)
-	 * @generated 
-	 *            "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	public void actualizarVentana(Object datos) {
-		// begin-user-code
-		// TODO Ap�ndice de m�todo generado autom�ticamente
+public class FrameAltaClientes extends JPanel implements GUIInterfazClientes {
 
-		// end-user-code
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public FrameAltaClientes() {
+
 	}
+
+	@Override
+	public void actualizarVentana(Object datos) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
