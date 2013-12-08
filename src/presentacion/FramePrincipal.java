3ca46d75f66fd2a732c5b7afa7f7023dee1b1c03
Plandevida <plandevida@planes.com>
2013-12-08 02:05:16 +0100

    Polladas con la GUI

--- src/presentacion/FramePrincipal.java
+++ src/presentacion/FramePrincipal.java
@@ -10,6 +10,20 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JToolBar;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.SwingConstants;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class FramePrincipal extends JFrame {
 
@@ -39,20 +53,63 @@ public class FramePrincipal extends JFrame {
 	public FramePrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 928, 743);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnArchivo = new JMenu("Archivo");
+		menuBar.add(mnArchivo);
+		
+		JMenuItem mntmSalir = new JMenuItem("Salir");
+		mntmSalir.setIcon(new ImageIcon(FramePrincipal.class.getResource("/images/icons/Salir16.png")));
+		mnArchivo.add(mntmSalir);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
 		JToolBar toolBar = new JToolBar();
+		toolBar.setAlignmentY(Component.CENTER_ALIGNMENT);
+		toolBar.setBackground(new Color(245, 245, 245));
+		toolBar.setMinimumSize(new Dimension(13, 23));
+		toolBar.setMaximumSize(new Dimension(150, 23));
 		contentPane.add(toolBar, BorderLayout.NORTH);
 		
+		Component glue = Box.createGlue();
+		toolBar.add(glue);
+		
 		JButton btnClientes = new JButton("Clientes");
-		
-		ImageIcon iconClientes = new ImageIcon(new ImageIcon(FramePrincipal.class.getResource("/images/icons/twister.png")).getImage().getScaledInstance(56, 56, Image.SCALE_FAST));
-		
-		btnClientes.setIcon(iconClientes);
+		btnClientes.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnClientes.setForeground(Color.BLACK);
+		btnClientes.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnClientes.setOpaque(false);
 		toolBar.add(btnClientes);
+		btnClientes.setVerticalTextPosition(SwingConstants.BOTTOM);
+		btnClientes.setMnemonic(KeyEvent.VK_C);
+		btnClientes.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnClientes.setToolTipText("Abre la sección de administración de clientes");
+		
+		btnClientes.setIcon(new ImageIcon(FramePrincipal.class.getResource("/images/icons/Clientes64.png")));
+		
+		Component glue_1 = Box.createGlue();
+		toolBar.add(glue_1);
 	}
 
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
 }
