88a00510a962ec28743c297067982e9fdb59078a
Plandevida <plandevida@planes.com>
2013-12-08 01:26:17 +0100

    Primeros intentos con los frames

---
+++ src/presentacion/FramePrincipal.java
@@ -0,0 +1,58 @@
+package presentacion;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JToolBar;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
+public class FramePrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FramePrincipal frame = new FramePrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FramePrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 928, 743);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JToolBar toolBar = new JToolBar();
+		contentPane.add(toolBar, BorderLayout.NORTH);
+		
+		JButton btnClientes = new JButton("Clientes");
+		
+		ImageIcon iconClientes = new ImageIcon(new ImageIcon(FramePrincipal.class.getResource("/images/icons/twister.png")).getImage().getScaledInstance(56, 56, Image.SCALE_FAST));
+		
+		btnClientes.setIcon(iconClientes);
+		toolBar.add(btnClientes);
+	}
+
+}
--- src/principal/HotelManager.java
+++ src/principal/HotelManager.java
@@ -1,10 +1,22 @@
 package principal;
 
+import java.awt.EventQueue;
+
+import presentacion.FramePrincipal;
+
 
 public class HotelManager {
 
 	public static void main(String[] args) {
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FramePrincipal frame = new FramePrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-
 }
