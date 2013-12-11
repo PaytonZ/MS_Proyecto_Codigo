3a9b7e0acf9c8a32be70b3b59852065cc9fb9d5e
emilalvap <emil.alvap@gmail.com>
2013-12-11 23:43:37 +0100

    GUI modificar reservas REVISAR TRATAMIENTO DE FECHAS
    estoy muy perro

---
+++ src/presentacion/comandos/reserva/CommandConsultarClienteModificarReserva.java
@@ -0,0 +1,30 @@
+package presentacion.comandos.reserva;
+
+import negocio.excepciones.BSoDException;
+import negocio.factorias.serviciosAplicacion.FactorySA;
+import presentacion.comandos.Command;
+import presentacion.comandos.IDEventos;
+import presentacion.comandos.commandFactory.CommandResponse;
+
+public class CommandConsultarClienteModificarReserva implements Command{
+
+
+	@Override
+	public CommandResponse execute(Object datos) {
+		CommandResponse cr = new CommandResponse();
+		try {
+
+			cr.setDatos(FactorySA.getInstance().getSAClientes().obtenerCliente((String)datos));
+			cr.setEvento(IDEventos.EVENTO_CONSULTAR_CLIENTE_V_MODIFICAR_RESERVAS);
+
+		} catch (BSoDException bsod) {
+
+			cr.setDatos(bsod);
+			cr.setEvento(IDEventos.ERROR_CONSULTAR_CLIENTE_V_MODIFICAR_RESERVAS);
+		}
+
+		return cr;
+	}
+}
+
+
