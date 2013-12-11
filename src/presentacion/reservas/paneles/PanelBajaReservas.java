8fd8cd07f2f5ff2951e9341319fad1361d083d7b
emilalvap <emil.alvap@gmail.com>
2013-12-11 01:39:23 +0100

    Guis
    panel de baja y modificacion
    sin codigo

--- src/presentacion/reservas/paneles/PanelBajaReservas.java
+++ src/presentacion/reservas/paneles/PanelBajaReservas.java
@@ -3,7 +3,13 @@
  */
 package presentacion.reservas.paneles;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 
 import presentacion.GUIPanelesInterfaz;
 import presentacion.comandos.IDEventos;
@@ -16,13 +22,75 @@ import presentacion.comandos.IDEventos;
  *            "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
  */
 public class PanelBajaReservas extends JPanel implements GUIPanelesInterfaz {
-	/**
-	 * (sin Javadoc)
-	 * 
-	 * @see GUIInterfazReservas#actualizarVentana()
-	 * @generated 
-	 *            "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
+
+	private JTextField textFechaSalida ;
+	private JTextField textDNI;
+	private JTextField textFechaEntrada; 
+	private JTextField textNHabitacion;
+	private JTextField textFechaReserva;
+	private JTextField textNReserva; 
+	
+	public PanelBajaReservas(){
+		setLayout(new MigLayout("", "[][][::164.00,grow][70.00][236.00]", "[][][][][][][][][][][][][][][]"));
+		
+		JLabel lblBajaDeReserva = new JLabel("Baja de reserva");
+		add(lblBajaDeReserva, "cell 3 0");
+		
+		JSeparator separator = new JSeparator();
+		add(separator, "cell 1 1 4 1, growx");
+		
+		JLabel lblNumeroDeReserva = new JLabel("Numero de reserva");
+		add(lblNumeroDeReserva, "cell 1 5,alignx right");
+		
+		textNReserva = new JTextField();
+		add(textNReserva, "cell 2 5,growx");
+		textNReserva.setColumns(10);
+		
+		JLabel lblFechaDeReserva = new JLabel("Fecha de reserva");
+		add(lblFechaDeReserva, "cell 3 5,alignx trailing");
+		
+		textFechaReserva = new JTextField();
+		add(textFechaReserva, "cell 4 5,growx");
+		textFechaReserva.setColumns(10);
+		
+		JLabel lblNumeroDeHabitacion = new JLabel("Numero de habitacion");
+		add(lblNumeroDeHabitacion, "cell 1 6,alignx right");
+		
+		textNHabitacion = new JTextField();
+		add(textNHabitacion, "cell 2 6,growx");
+		textNHabitacion.setColumns(10);
+		
+		JLabel lblFechaEntrada = new JLabel("Fecha Entrada");
+		add(lblFechaEntrada, "cell 3 6,alignx trailing");
+		
+		textFechaEntrada = new JTextField();
+		add(textFechaEntrada, "cell 4 6,growx");
+		textFechaEntrada.setColumns(10);
+		
+		JLabel lblDni = new JLabel("DNI");
+		add(lblDni, "cell 1 7,alignx trailing");
+		
+		textDNI = new JTextField();
+		add(textDNI, "cell 2 7,growx");
+		textDNI.setColumns(10);
+		
+		JLabel lblFechaDeSalida = new JLabel("Fecha de salida");
+		add(lblFechaDeSalida, "cell 3 7,alignx trailing");
+		
+		textFechaSalida = new JTextField();
+		add(textFechaSalida, "cell 4 7,growx");
+		textFechaSalida.setColumns(10);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		add(btnBuscar, "cell 1 9");
+		
+		JButton btnBorrar = new JButton("Borrar");
+		add(btnBorrar, "cell 4 9,alignx right");
+		
+		JSeparator separator_1 = new JSeparator();
+		add(separator_1, "cell 0 11 5 1,growx");
+
+	}
 	public void actualizarVentana(IDEventos idEventos, Object datos) {
 		// begin-user-code
 		// TODO Ap�ndice de m�todo generado autom�ticamente
--- src/presentacion/reservas/paneles/PanelModificacionReservas.java
+++ src/presentacion/reservas/paneles/PanelModificacionReservas.java
@@ -3,7 +3,15 @@
  */
 package presentacion.reservas.paneles;
 
+import java.awt.Button;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 
 import presentacion.GUIPanelesInterfaz;
 import presentacion.comandos.IDEventos;
@@ -16,13 +24,71 @@ import presentacion.comandos.IDEventos;
  *            "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
  */
 public class PanelModificacionReservas extends JPanel implements GUIPanelesInterfaz {
-	/**
-	 * (sin Javadoc)
-	 * 
-	 * @see GUIInterfazReservas#actualizarVentana()
-	 * @generated 
-	 *            "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
+
+	private JTextField textID;
+	private JTextField textDNI;
+	private JTextField textFechaSalida;
+	private JTextField textNHabitacion;
+	private JTextField textFechaEntrada;
+	public PanelModificacionReservas(){
+setLayout(new MigLayout("", "[53.00][123.00,grow][47.00][51.00][48.00][56.00][189.00]", "[][][][][][][][][][][][][][][]"));
+		
+		JLabel lblModificacionDeReserva = new JLabel("Modificacion de reserva");
+		add(lblModificacionDeReserva, "cell 3 0 3 1");
+		
+		JSeparator separator = new JSeparator();
+		add(separator, "cell 0 1 7 1,growx");
+		
+		JLabel lblId = new JLabel("ID");
+		add(lblId, "cell 0 3,alignx trailing");
+		
+		textID = new JTextField();
+		add(textID, "cell 1 3,growx");
+		textID.setColumns(10);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		add(btnBuscar, "cell 3 3");
+		
+		JButton btnNBuscar = new JButton("Nueva busqueda");
+		add(btnNBuscar, "cell 5 3");
+		
+		JSeparator separator_1 = new JSeparator();
+		add(separator_1, "cell 0 5 7 1, growx");
+		
+		JLabel lblDni = new JLabel("DNI");
+		add(lblDni, "cell 0 7,alignx trailing");
+		
+		textDNI = new JTextField();
+		add(textDNI, "cell 1 7,growx");
+		textDNI.setColumns(10);
+		
+		JLabel lblFechaSalida = new JLabel("Fecha Salida");
+		add(lblFechaSalida, "cell 5 7,alignx trailing");
+		
+		textFechaSalida = new JTextField();
+		add(textFechaSalida, "cell 6 7,growx");
+		textFechaSalida.setColumns(10);
+		
+		JLabel lblFechaReserva = new JLabel("Fecha Reserva");
+		add(lblFechaReserva, "cell 0 8,alignx trailing");
+		
+		textNHabitacion = new JTextField();
+		add(textNHabitacion, "cell 1 8,growx");
+		textNHabitacion.setColumns(10);
+		
+		JLabel lblFechaEntrada = new JLabel("Fecha Entrada");
+		add(lblFechaEntrada, "cell 5 8,alignx trailing");
+		
+		textFechaEntrada = new JTextField();
+		add(textFechaEntrada, "cell 6 8,growx");
+		textFechaEntrada.setColumns(10);
+		
+		JSeparator separator_2 = new JSeparator();
+		add(separator_2, "cell 0 10 7 1, growx");
+		
+		JButton btnModificar = new JButton("Modificar");
+		add(btnModificar, "cell 6 11,alignx right");
+	}
 	public void actualizarVentana(IDEventos idEventos, Object datos) {
 		// begin-user-code
 		// TODO Ap�ndice de m�todo generado autom�ticamente
