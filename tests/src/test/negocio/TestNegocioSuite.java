5e913ef8fbd9b3e3fe850ca260acbd0e620c35d9
PaytonZ <jlpv_90@hotmail.com>
2014-01-24 19:42:50 +0100

    Test.

--- tests/src/test/negocio/TestNegocioSuite.java
+++ tests/src/test/negocio/TestNegocioSuite.java
@@ -5,11 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import test.negocio.clientes.servicioaplicacion.imp.TestSAClientesImp;
+import test.negocio.empleados.servicioaplicacion.imp.TestSAEmpleadosImp;
 import test.negocio.habitaciones.servicioaplicacion.imp.TestSAHabitacionesImp;
+import test.negocio.jpa.TestJPAEmpleado;
 import test.negocio.reservas.servicioaplicacion.imp.TestSAReservasimp;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestSAClientesImp.class, TestSAHabitacionesImp.class, TestSAReservasimp.class})
+@SuiteClasses({ TestSAClientesImp.class, TestSAHabitacionesImp.class, TestSAReservasimp.class , TestJPAEmpleado.class , TestSAEmpleadosImp.class})
 public class TestNegocioSuite {
 
 	// Nothing to do here
